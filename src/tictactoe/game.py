b06163b76401e8e7193fb3fed817c7c17e9c6fa0
RiazulRahat <riazul.rahat.uni@gmail.com>
2025-06-11 21:05:54 -0400

    " Modularized + Separating BE and FE + Added End Game Text"

--- src/tictactoe/game.py
+++ src/tictactoe/game.py
@@ -14,12 +14,9 @@ FRAMERATE = 60
 CIRCLE_IMG_PATH = "src/tictactoe/assets/circle.svg.png"
 CROSS_IMG_PATH = "src/tictactoe/assets/cross.svg.png"
 
-CIRCLE_TURN = True
+GAME_END_DELAY = 1000
 
-GAME_END_DELAY = 300
-
-# ----------------------------------------------------------------------
-
+# --------------------------
 # Initialize a new board
 board = ttt()
 # --------------------------
@@ -50,11 +47,25 @@ def drawGrid(boxSize):
     # horizontal line
     for y in range(boxSize, HEIGHT, boxSize):
         pygame.draw.line(SCREEN, lineColor, (0,y), (WIDTH,y))
+
+def game_over_text():
+    font = pygame.font.SysFont('Arial', 120)
+    textSurface = font.render(board.print_winner(), True, pygame.Color('darkseagreen'))
+    textRect = textSurface.get_rect(center=(WIDTH//2, HEIGHT//2))
+    SCREEN.blit(textSurface, textRect)
+
 # -----------------------------------------------------------------------
 
 # Image Loading
 circle, circleRect = loadImage(CIRCLE_IMG_PATH)
 cross, crossRect = loadImage(CROSS_IMG_PATH)
+
+# Populate image for pygame screen
+image_list = []
+def add_image():
+    image = circle if board.max_turn else cross
+    imageRect = image.get_rect(center=(col * CELL_SIZE, row * CELL_SIZE))
+    image_list.append((image, imageRect))
 
 # Main Game Loop --------------------------------------------------------
 running = True
@@ -67,32 +78,35 @@ while running:
             
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left mouse button clicks only 
                 x, y = event.pos
-                # print col and row
-                col = (x // CELL_SIZE)
+                # print row and col
                 row = (y // CELL_SIZE)
-
+                col = (x // CELL_SIZE)
                 coord = (row,col)
 
-                if board.contains[row][col] == '-':
-                    board.contains[row][col] = 'O' if CIRCLE_TURN else 'X'
-                    image = circle if CIRCLE_TURN else cross
-                    imageRect = image.get_rect(center=(
-                        col * CELL_SIZE,
-                        row * CELL_SIZE
-                        ))
-                    board.moves.append((image, imageRect, coord))
-                    CIRCLE_TURN = not CIRCLE_TURN
+                legal = list(board.legal_moves())
+                if coord in legal:
+                    add_image()
+                    board.make_nove((row,col))
+
+        # remove move by pressing 'b'
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+            if len(image_list) > 0:
+                image_list.pop()
+                board.remove_move()
+    
     # draw grid background
     drawGrid(CELL_SIZE)
 
     # Draw moves
-    for image, imageRect, _unusedCoord in board.moves:
+    for image, imageRect in image_list:
         SCREEN.blit(image, imageRect.center)
 
     pygame.display.update()
     clock.tick(FRAMERATE)
 
     if board.is_game_over():
+        game_over_text()
+        pygame.display.update()
         pygame.time.delay(GAME_END_DELAY)
         running = False
 
--- src/tictactoe/ttt_engine.py
+++ src/tictactoe/ttt_engine.py
@@ -4,18 +4,22 @@
 
 class GS_tictactoe:
 
-    # initialize contains state in the beginning
     def __init__(self):
+        # current board state
         self.contains = [['-','-','-'],
                          ['-','-','-'],
                          ['-','-','-']]
         
+        # contains stack of moves leading up to current board state
         self.moves = []
+        self.winner = '-'
+        self.max_turn = True
 
     # Private Functions
     def _rowCheck(self):
         for row in self.contains:
             if (row[0] != '-') and (row[0] == row[1] == row[2]):
+                self.winner = row[0]
                 return True
             
         return False
@@ -23,21 +27,51 @@ class GS_tictactoe:
     def _colCheck(self):
         for col in range(3):
             if (self.contains[0][col] != '-') and (self.contains[0][col] == self.contains[1][col] == self.contains[2][col]):
+                self.winner = self.contains[0][col]
                 return True
-            
         return False
     
     def _diagCheck(self):
         if (self.contains[0][0] != '-') and (self.contains[0][0] == self.contains[1][1] == self.contains[2][2]):
+            self.winner = self.contains[0][0]
             return True
         if (self.contains[0][2] != '-') and (self.contains[2][0] == self.contains[1][1] == self.contains[0][2]):
+            self.winner = self.contains[2][0]
             return True
-        
         return False
-    
+    # ------------------
+
     def is_game_over(self):
-        if len(self.moves) == 9:
-            return True
-        
-        return self._rowCheck() or self._colCheck() or self._diagCheck()
+        '''Check if game is over!'''
+        return self._rowCheck() or self._colCheck() or self._diagCheck() or (len(self.moves) == 9)
     
+    def remove_move(self):
+        '''Remove last move'''
+        if len(self.moves) == 0:
+            return None
+        move = self.moves.pop()
+        self.contains[move[0]][move[1]] = '-'
+
+    def print_winner(self):
+        if self.winner == 'X':
+            return "X wins!"
+        elif self.winner == 'O':
+            return "O wins!"
+        else:
+            return "Draw!"
+    
+    def make_nove(self, move):
+        if self.contains[move[0]][move[1]] == '-':
+            self.contains[move[0]][move[1]] = 'O' if self.max_turn else 'X'
+            self.moves.append(move)
+            self.max_turn = not self.max_turn
+
+    def legal_moves(self):
+
+        movesList = []
+        for row in range(3):
+            for col in range(3):
+                if self.contains[row][col] == '-':
+                    movesList.append((row,col))
+
+        return movesList
